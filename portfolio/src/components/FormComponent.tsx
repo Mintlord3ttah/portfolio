ed98c822239de4b9adda22cdea5e601d1d6978e5
Mintlord3ttah <lordmint7@gmail.com>
2025-07-10 09:28:11 +0100

    uin fix

--- portfolio/src/components/FormComponent.tsx
+++ portfolio/src/components/FormComponent.tsx
@@ -1,9 +1,9 @@
 
 export default function FormComponent() {
   return <form action={"https://formspree.io/f/xkgblybk"} method="POST" className='flex flex-col items-center w-full max-w-md mx-auto mt-8'>
-  <input type="text" name="name" placeholder="Your Name" className='border border-gray-300 p-2 mb-4 w-full' />
-  <input type="email" name="email" placeholder="Your Email" className='border border-gray-300 p-2 mb-4 w-full' />
-  <textarea name="message" placeholder="Your Message" className='border resize-none border-gray-300 p-2 mb-4 w-full' rows={4} />
+  <input type="text" name="name" required placeholder="Your Name" className='border border-gray-300 p-2 mb-4 w-full' />
+  <input type="email" name="email" required placeholder="Your Email" className='border border-gray-300 p-2 mb-4 w-full' />
+  <textarea name="message" required placeholder="Your Message" className='border resize-none border-gray-300 p-2 mb-4 w-full' rows={4} />
   <button className="mt-6 px-6 py-2 bg-[#c46b31] cursor-pointer text-white rounded-full hover:bg-amber-600 transition duration-300">Send Message</button>
   </form>
 }
