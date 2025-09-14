c36537da07a45b6e68b7de68373126b110a539a5
Mintlord3ttah <lordmint7@gmail.com>
2025-09-14 11:13:08 +0100

    add new changes

--- portfolio/src/components/ProjectCard.tsx
+++ portfolio/src/components/ProjectCard.tsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
 import { useAppProvider } from "../themeContext/AppProvider";
+import { FaStar } from "react-icons/fa";
 
-export default function ProjectCard({img, title, description, link} : {img: string, title: string, description: string, link?: string}) {
+export default function ProjectCard({img, title, description, link, rating} : {rating: string, img: string, title: string, description: string, link?: string}) {
   const {theme} = useAppProvider()
+
+  function stringTruncate(str: string) {
+    return str.split("/").at(-1);
+  }
   return <Link to={link || "#"} >
-    <div className={`pro-hover ${theme === "light" ? "bg-white/45" : "bg-gray-900/50"} shrink-0 backdrop-blur-lg z-10 shadow-lg flex flex-col gap-4 w-[17rem] max-[490px]:w-full group rounded-2xl p-2.5 cursor-pointer transition-all duration-300`}>
+    <div className={`pro-hover relative ${theme === "light" ? "bg-white/45" : "bg-gray-900/50"} shrink-0 backdrop-blur-lg z-10 shadow-lg flex flex-col gap-4 w-[17rem] max-[490px]:w-full group rounded-2xl p-2.5 cursor-pointer transition-all duration-300`}>
     <img src={img} alt="project photo" className='w-full h-[9rem] rounded-2xl group-hover:scale-105 transition-all duration-500' />
     <p className="font-bold">{title}</p>
     <p>{description}</p>
-    <p className="text-sm text-[var(--primary-color)] italic mb-4">site: {link}</p>
+    <div className="flex flex-col justify-end flex-1 py-4">
+    <p className="text-sm text-[var(--primary-color)] italic mb-4">site: {stringTruncate(link || "")}</p>
+      <div className={`flex items-center text-sm text-gray-500 border ${theme === "light" ? "border-gray-300 bg-gray-200" : "border-gray-700 bg-gray-800"} rounded-full w-fit px-6 py-2 cursor-pointer`}>
+      <span className="mr-2.5 border-r border-r-gray-500 pr-2.5"><FaStar /> </span>
+      <span className="m-">{rating}</span>
+      </div>
+    </div>
   </div>
     </Link>
 }
--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -40,7 +40,7 @@ export default function Home() {
                     </div>
                     <div className="">
                         <h1 className="text-4xl font-bold mt-6 text-[#c46b31]">Confidence N. Ettah</h1>
-                        <p className="text-lg max-[1000px]:text-2xl mt-4">Hi, I'm a MERN-Stack/Mobile App developer with a passion for creating beautiful and functional user experiences.</p>
+                        <p className=" max-[1000px]:text-2xl mt-4 font-medium">I deliver quality, user-friendly experiences like e-commerce websites, school / orginanizational websites, portfolios, dashbpoards etc.</p>
                         <CTA onClick={() => setIsScroll(prev => !prev)} />
                     </div>
                     <div className="mt-24 relative">
@@ -54,7 +54,7 @@ export default function Home() {
                     <BG_Glow size={50} color={{ one: "bg-green-300", two: "bg-green-400", three: "bg-green-500" }} position="right-1/2" />
                     <div className=" backdrop-blur-xs z-10 flex  justify-end flex-col gap-4">
                         <h2 className="text-[2.5rem] max-[330px]:text-4xl font-bold bg-gradient-to-r from-blue-600 to-orange-700 text-clip bg-clip-text text-transparent">Hey, I'm Glad You Are Here!</h2>
-                        <p className="text-lg max-[1000px]:text-2xl text-gray-500">This is a simple portfolio application built with React & TypeScript. Feel free to explore my projects and skills!</p>
+                        <p className="text-lg max-[350px]:text-[1.1rem] text-gray-500">I can create for you a full fledged and beautiful functional user-friendly website or mobile application using mordern technologies like React, React Native, Next.js, TypeScript etc. Feel free to explore my projects and skills!</p>
                         <div className="flex gap-4 flex-wrap">
                             <CTA onClick={() => setIsScroll(prev => !prev)} />
                             <Link to={"/about"} onClick={() => setSpecNavigation(true)} className={`mt-6 w-fit px-6 py-2  border ${theme === "light" ? "border-gray-500 text-gray-700 hover:bg-gray-400" : "hover:bg-[#111] border-gray-600 text-gray-300"} cursor-pointer rounded-full transition duration-300`}>My Services</Link>
@@ -77,10 +77,12 @@ export default function Home() {
                     <BG_Glow size={50} color={{ one: "bg-blue-300", two: "bg-blue-400", three: "bg-blue-500" }} position="right-[50%] bottom-[20%]" />
                     <h2 className="text-2xl  mb-8 text-[var(--primary-color)]">Projects</h2>
                     <div className="grid grid-cols-4 max-[1100px]:grid-cols-2 max-[636px]:gap-4 max-[600px]:grid-cols-1 gap-8 w-fit">
-                        <ProjectCard img="iREMEMBER APP-2.gif" title="Iremember - Task Management app" description="This is a simple yet indespensible web app for preparing and managing travel items." link="https://iremember-eta.vercel.app" />
-                        <ProjectCard img="new design insta.png" title="Instablog - Blogging Platform" description="This is a blogging platform that allows users to create and share their own blogs." link="https://instablog.vercel.app" />
-                        <ProjectCard img="mapify2.png" title="Mapify - Location Tracker" description="This project is a map based web app that helps users keep logs/track their location." link="https://mapify.com" />
-                        <ProjectCard img="fundaweb-best-landing-2.png" title="Fundaweb - Collaborative investment app" description="This is a collaborative investment app that allows users to pool resources and invest in real estate projects." link="https://fundaweb.com" />
+                        <ProjectCard rating="4.5" img="iREMEMBER APP-2.gif" title="Iremember - Task Management app" description="This is a simple yet indespensible web app for preparing and managing travel items." link="https://iremember-eta.vercel.app" />
+                        <ProjectCard rating="4.5" img="dailybread-foods-1.png" title="Foodie - food delivery" description="This is a platform for delivering food from local restaurants." link="https://mintlord3ttah.github.io/dailybreadfood.com" />
+                        <ProjectCard rating="4.2" img="chef-diary.png" title="Chef Diary - Search All Recipes" description="A platform to seemlessly grab any kind of recipe with auto completion and search filtering" link="https://chef-diary.vercel.app" />
+                        <ProjectCard rating="3.5" img="new design insta.png" title="Instablog - Blogging Platform" description="This is a blogging platform that allows users to create and share their own blogs." link="https://instablog.vercel.app" />
+                        <ProjectCard rating="4.0" img="mapify2.png" title="Mapify - Location Tracker" description="This project is a map based web app that helps users keep logs/track their location." link="https://mapify.com" />
+                        <ProjectCard rating="4.5" img="fundaweb-best-landing-2.png" title="Fundaweb - Collaborative investment app" description="This is a collaborative investment app that allows users to pool resources and invest in real estate projects." link="https://fundaweb.com" />
                     </div>
                 </div>
             </section>
