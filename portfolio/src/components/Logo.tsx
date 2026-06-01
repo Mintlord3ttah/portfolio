f2f3c51478c4aa47ca2619dde25692a75e6f58a3
Mintlord3ttah <mintlordettah@gmail.com>
2026-06-01 21:41:55 +0100

    Update Logo component to use company logo
    Replaced placeholder image with company logo and commented out unused elements.

--- portfolio/src/components/Logo.tsx
+++ portfolio/src/components/Logo.tsx
@@ -5,12 +5,12 @@ export default function Logo() {
   return (
     <Link to={"/"}>
     <div className="flex items-center gap-2 text-2xl font-bold">
-        {/* <img src="/icon.jfif" alt="Logo" className="size-12 rounded-full" /> */}
-        <h3 className="text-4xl text-[#c46b31]"><IoIosFlash /></h3>
+        <img src="/companyname.png" alt="Logo" className="h-12" />
+      {/*<h3 className="text-4xl text-[#c46b31]"><IoIosFlash /></h3>
         <span className="font-bold flex text-[#c46b31]">
             <span className=" py-1.5">code</span>
             <span className="flex items-center justify-center text-gray-700">split</span>
-        </span>
+        </span>*/}
     </div>
     </Link>
   )
